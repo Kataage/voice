8b1b8519ef128dcdb8f7d2d9f2ab084510400456
Kataage <50143135+Kataage@users.noreply.github.com>
2026-08-22 18:27:41 +0900

    style: simplify Irodori revision test import

--- tests/test_irodori_v41_revision.py
+++ tests/test_irodori_v41_revision.py
@@ -1,4 +1,4 @@
-from personavoice.model_assets import IRODORI_MODEL_ID, IRODORI_MODEL_REVISION, IRODORI_MODEL_SHA256
+from personavoice import model_assets
 
 
 V4_MODEL_REVISION = "c0097bd1be75833b44498bb6fcf0bee9298262a7"
@@ -7,7 +7,7 @@ V41_MODEL_SHA256 = "c85de88c01700cb53538e706f128ebcb1b8513ad21d7d0e75f58bc82cdbf
 
 
 def test_irodori_v41_uses_its_own_huggingface_revision() -> None:
-    assert IRODORI_MODEL_ID == "Aratako/Irodori-TTS-v4.1-Small"
-    assert IRODORI_MODEL_REVISION == V41_MODEL_REVISION
-    assert IRODORI_MODEL_REVISION != V4_MODEL_REVISION
-    assert IRODORI_MODEL_SHA256 == V41_MODEL_SHA256
+    assert model_assets.IRODORI_MODEL_ID == "Aratako/Irodori-TTS-v4.1-Small"
+    assert model_assets.IRODORI_MODEL_REVISION == V41_MODEL_REVISION
+    assert model_assets.IRODORI_MODEL_REVISION != V4_MODEL_REVISION
+    assert model_assets.IRODORI_MODEL_SHA256 == V41_MODEL_SHA256
